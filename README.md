4f550a1edf2cfc479e44e5c6b371212f403f3445
metafates <vvz.dev@gmail.com>
2025-08-01 21:12:53 +0300

    add godoc

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
-# Envvar
+# Ev
 
-Go library to create typed environment variables.
+> E(nvironment) v(ariable)
+
+Tiny Go library to create typed environment variables.
+
+```bash
+go get github.com/metafates/ev
+```
+
+## Example
+
+```go
+const MyEnvVar ev.Var[int] = "MY_ENV_VAR"
+
+const Verbose ev.Var[bool] = "VERBOSE"
+
+func main() {
+    // assume we have the following variables set
+    os.Setenv("MY_ENV_VAR", "42")
+    os.Setenv("VERBOSE", "true")
+
+    if Verbose.Get() {
+        n := MyEnvVar.Get()
+
+        fmt.Println(n + n) // prints 84
+    }
+}
+```
+
+Supported types:
+
+``` go
+type Constraint interface {
+    int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | uintptr | float32 | float64
+}
+```
+
+Values are parsed using `fmt.Sscanf`.
--- ev.go
+++ ev.go
@@ -12,6 +12,9 @@ var (
 	mu            sync.RWMutex
 )
 
+// SetDefault sets default value for the given var.
+//
+// This function is safe to call from multiple goroutines.
 func SetDefault[T Constraint](v Var[T], value T) {
 	mu.Lock()
 	defer mu.Unlock()
@@ -33,12 +36,21 @@ func getDefault[T Constraint](v Var[T]) T {
 	return zero
 }
 
+// Constraint defines all supported types for [Var].
+//
+// It does not permit ~T (tilde) types since other types may be parsed
+// differently, which may result unexpected behavior.
 type Constraint interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr | ~float32 | ~float64
+	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | uintptr | float32 | float64
 }
 
+// Var is an environment variable.
+//
+// T defines which value type this is expected by this variable.
 type Var[T Constraint] string
 
+// Or returns the first of its arguments for which [Var.TryGet] returns ok == true.
+// If no argument is suitable, it returns the zero value.
 func Or[T Constraint](vars ...Var[T]) T {
 	for _, v := range vars {
 		value, ok := v.TryGet()
@@ -54,6 +66,8 @@ func Or[T Constraint](vars ...Var[T]) T {
 
 func (v Var[T]) get() string { return os.Getenv(string(v)) }
 
+// GetOr returns the first of its arguments that is not equal to the zero value.
+// If no argument is non-zero, it returns the zero value.
 func (v Var[T]) GetOr(or ...T) T {
 	value, ok := v.TryGet()
 	if !ok {
@@ -63,6 +77,10 @@ func (v Var[T]) GetOr(or ...T) T {
 	return value
 }
 
+// Get returns the value of this variable.
+// It value is missing or not scannable by [fmt.Sscanf] it returns default value.
+//
+// See also [SetDefault] to set a default value.
 func (v Var[T]) Get() T {
 	value, ok := v.TryGet()
 	if !ok {
@@ -72,6 +90,8 @@ func (v Var[T]) Get() T {
 	return value
 }
 
+// TryGet returns a value of this variable and boolean
+// stating whether value was present and [fmt.Sscanf] successfully scanned it.
 func (v Var[T]) TryGet() (T, bool) {
 	raw := v.get()
 	if raw == "" {
